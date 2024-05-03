72385dc8fa7b25b6f4a6e4c10cb9d94d37a5d2a5
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-05-03 22:30:21 +0100

    feat(creacion-clases):agregacion metodo para la clase menu

---
+++ src/net/salesianos/supermercado/utilidad/Menu.java
@@ -0,0 +1,16 @@
+package net.salesianos.supermercado.utilidad;
+
+public class Menu {
+    public static String menu() {
+        return "=======================================\n" +
+                "|Menu:                                |\n"
+                + "=======================================\n" +
+                "|1. Abrir caja                        |\n" +
+                "|2. Añadir un nuevo cliente a la cola |\n" +
+                "|3. Atender un cliente                |\n" +
+                "|4. Ver clientes pendientes           |\n" +
+                "|5. Cerrar supermercado               |\n" +
+                "=======================================";
+
+    }
+}
