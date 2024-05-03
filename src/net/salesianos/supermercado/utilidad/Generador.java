063a1069dad8d98790e7ebc8c27bb6bad7a4764e
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-05-03 22:39:46 +0100

    feat: agregacion atributos en la clase generador , arrayslist con nombres y productos

---
+++ src/net/salesianos/supermercado/utilidad/Generador.java
@@ -0,0 +1,15 @@
+package net.salesianos.supermercado.utilidad;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Generador {
+     private static ArrayList<String> nombresDisponibles = new ArrayList<>(Arrays.asList("Maria ", "Mario ", "Joaquim ",
+            "Martin",
+            "Jose", "Laura", "Pablo", "sofia",
+            "Adrian", "Elena", "David", "Olivera", "Javier", "carmen", "Pepe", "Raquel", "Sara", "Juan", "Israel",
+            "Alonso"));
+    private static ArrayList<String> productosDisponibles = new ArrayList<>(Arrays.asList(
+            "Pan", "Leche", "Huevos", "Arroz", "Chuletas", "Cebolla", "Ajo",
+            "Banano", "Pollo", "Carne ", "Atun", "Nuggets"));
+}
