a31b661029683eed248bd037e0aaf8e454a9f952
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-05-03 22:40:28 +0100

    feat: creacion metodos para generar nombres y productos aleatorios

--- src/net/salesianos/supermercado/utilidad/Generador.java
+++ src/net/salesianos/supermercado/utilidad/Generador.java
@@ -2,9 +2,10 @@ package net.salesianos.supermercado.utilidad;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Generador {
-     private static ArrayList<String> nombresDisponibles = new ArrayList<>(Arrays.asList("Maria ", "Mario ", "Joaquim ",
+    private static ArrayList<String> nombresDisponibles = new ArrayList<>(Arrays.asList("Maria ", "Mario ", "Joaquim ",
             "Martin",
             "Jose", "Laura", "Pablo", "sofia",
             "Adrian", "Elena", "David", "Olivera", "Javier", "carmen", "Pepe", "Raquel", "Sara", "Juan", "Israel",
@@ -12,4 +13,16 @@ public class Generador {
     private static ArrayList<String> productosDisponibles = new ArrayList<>(Arrays.asList(
             "Pan", "Leche", "Huevos", "Arroz", "Chuletas", "Cebolla", "Ajo",
             "Banano", "Pollo", "Carne ", "Atun", "Nuggets"));
+    private static Random random = new Random();
+
+    public static String generarProductoAleatorio() {
+        int index = random.nextInt(productosDisponibles.size());
+        return productosDisponibles.get(index);
+    }
+
+    public static String generarNombreAleatorio() {
+        int index = random.nextInt(nombresDisponibles.size());
+        return nombresDisponibles.get(index);
+    }
+
 }
